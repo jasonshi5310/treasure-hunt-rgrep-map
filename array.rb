9da87c58df90cc389af4c4bff584d9aebe35de90
nishikata-waifu <52653024+nishikata-waifu@users.noreply.github.com>
2020-10-15 14:57:58 -0400

    [] done

--- array.rb
+++ array.rb
@@ -1,3 +1,13 @@
 # array.rb 
 # cse 337 hw2
 # Minqi Shi 111548035
+
+class Array
+    alias old_bracket []
+    def [](i)
+        element = '\0'
+        actual = old_bracket(i)
+        actual != nil ? (element = actual) : element
+        element 
+    end
+end
