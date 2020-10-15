723530f0e14d21fbdac8e6e4534b8eec6e232331
nishikata-waifu <52653024+nishikata-waifu@users.noreply.github.com>
2020-10-15 19:00:37 -0400

    map(*range) is done
    Unlimited if works LOL

--- array.rb
+++ array.rb
@@ -8,6 +8,30 @@ class Array
         element = '\0'
         actual = old_bracket(i)
         actual != nil ? (element = actual) : element
-        element 
+    end
+
+    alias old_map map
+    def map(*range, &block)
+        if range.length() == 0
+            self.old_map(&block)
+        else
+            is_valid = true
+            if range.length() != 1
+                is_valid = false
+            end
+            if !range[0].instance_of?(Range)
+                is_valid = false
+            end
+            if is_valid == false
+                Array.new
+            else
+                a = self.old_bracket(range[0])
+                if a == nil
+                    Array.new
+                else
+                    a.old_map(&block)
+                end
+            end
+        end
     end
 end
