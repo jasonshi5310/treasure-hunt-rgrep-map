dceb5f76ecb6c3e9c98f2f60a98a959a783edcde
nishikata-waifu <52653024+nishikata-waifu@users.noreply.github.com>
2020-10-16 00:16:04 -0400

    handled invalid cases for part 2

---
+++ rgrep.rb
@@ -0,0 +1,99 @@
+# Inplementation of grep in Ruby
+# Minqi Shi
+
+MISSING_REQUIRED_ARGUMENTS = 'Missing required arguments'
+# NO_SUCH_FILE_OR_DIRECTORY = 'No such file or directory'
+INVALID_OPTION = 'Invalid option'
+INVALID_COMBINATION_OF_OPTIONS = 'Invalid combination of options'
+
+class NoFileError<StandardError
+    def msg()
+        MISSING_REQUIRED_ARGUMENTS
+    end
+end
+
+class InvalidOptionError<StandardError
+    def msg()
+        INVALID_OPTION
+    end
+end
+
+class InvalidCombinationError<StandardError
+    def msg()
+        INVALID_COMBINATION_OF_OPTIONS
+    end
+end
+
+
+puts 'ARGV:'
+for arg in ARGV
+    puts arg
+end
+puts '-----------------------------------------'
+
+if ARGV.length() < 2
+    puts MISSING_REQUIRED_ARGUMENTS
+    exit(1)
+end
+
+if ARGV.length() > 4
+    puts INVALID_COMBINATION_OF_OPTIONS
+    exit(1)
+end
+
+begin
+    args = ARGV[1..-1]
+    filename = ARGV[0]
+    raise NoFileError if !File.file?(filename)
+    for option in args[0..-2]
+        if option.length() != 2
+            raise InvalidOptionError
+        end
+        dash = option[0]
+        op = option[1]
+        if dash != '-' or (op != 'w' and op != 'p' and op != 'v' and op != 'c' and op != 'm')
+            raise InvalidOptionError
+        end
+    end
+    if args.length() == 3
+        if args[0] == '-c'
+            if args[1] == '-c' or args[1] = '-m'
+                raise InvalidCombinationError
+            end
+        elsif args[1] == '-c'
+            if args[0] == '-c' or args[0] = '-m'
+                raise InvalidCombinationError
+        end
+        if args[0] == '-m'
+            if args[1] != '-w' and args[1] != '-p'
+                raise InvalidCombinationError
+            end
+        elsif args[1] == '-m'
+            if args[0] != '-w' and args[0] != '-p'
+                raise InvalidCombinationError
+            end
+        end
+    else
+        raise InvalidCombinationError
+    end
+    File.open(filename, "r") do |file|
+        puts 'File content:'
+        for line in file
+            puts line
+        end
+        puts '-----------------------------------------'
+    end
+    # else
+    #     raise NoFileError
+    # end
+rescue NoFileError => e
+    puts e.msg
+    exit(1)
+rescue InvalidOptionError => e
+    puts e.msg
+    exit(1)
+rescue InvalidCombinationError => e
+    puts e.msg
+    exit(1)
+end
+
