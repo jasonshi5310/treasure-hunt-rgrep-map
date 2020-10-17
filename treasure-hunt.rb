8dec2a0ddc4aadbaca3492fbed78965cc1b418a7
nishikata-waifu <52653024+nishikata-waifu@users.noreply.github.com>
2020-10-17 01:25:50 -0400

    finished room

--- treasure-hunt.rb
+++ treasure-hunt.rb
@@ -72,3 +72,116 @@ class Narrator
     say @ending_message
   end
 end
+
+
+# -------------------------------------------------------------
+
+SAFE = 0 # 'safe'
+# BATS = 1
+# NEIGHBORS_BATS = 2
+# PIT = 2
+# GUARD = 3
+# NEIGHBORS_GUARD = 4
+NOT_SAFE = 5
+
+class Room
+    def initialize(number, guard=false, bats=false, pit=false, neighbors=Array.new, is_safe = SAFE)
+        @number = number
+        @guard = guard
+        @bats = bats
+        @pit = pit
+        @neighbors = neighbors
+        @is_safe = is_safe
+    end
+    attr_accessor :number, :bats, :guard, :pit, :neighbors, :is_safe
+
+    def empty?
+        @is_safe == SAFE
+    end
+
+    def add(hazard)
+        if hazard.instance_of?(Symbol)
+            is_there = true
+            if hazard == :guard
+                @guard = true
+            elsif hazard == :pit
+                @pit = true
+            elsif hazard == :bats
+                @bats = true
+            else
+                is_there = false
+            end
+            # self.method(hazard).call
+            if is_there
+                @is_safe = NOT_SAFE
+            end
+        end
+    end
+
+    def remove(hazard)
+        if hazard.instance_of?(Symbol)
+            # self.method(hazard).call
+            if hazard == :guard
+                @guard = false
+            elsif hazard == :pit
+                @pit = false
+            elsif hazard == :bats
+                @bats = false
+            end
+            if @guard == false and @pit == false and @bats == false
+                @is_safe = SAFE
+            end
+        end
+    end
+
+    def has?(hazard)
+        if hazard.instance_of?(Symbol)
+            if hazard == :guard
+                @guard
+            elsif hazard == :pit
+                @pit
+            elsif hazard == :bats
+                @bats
+            else
+                false
+            end
+        else
+            false
+        end
+    end
+
+    def connect(room)
+        if room.instance_of?(Room)
+            @neighbors.push(room)
+            room.neighbors.push(self)
+        end
+    end
+
+    def exits
+        @neighbors.collect {|room| room.number}
+    end
+
+    def random_neighbor
+        random_index = Random.new.rand(@neighbors.length())
+        @neighbors[random_index]
+    end
+
+    def safe?
+        if @is_safe == NOT_SAFE
+            false
+        else
+            isSafe = true
+            for room in neighbors
+                if room.is_safe == NOT_SAFE
+                    isSafe = false
+                    break
+                end
+            end
+            isSafe
+        end
+    end
+
+    def neighbor(i)
+        @neighbors.select {|room| room.number == i} [0]
+    end
+end
